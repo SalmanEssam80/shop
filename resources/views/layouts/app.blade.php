5669fc9e511781f576ecd6d8614eec1aafcca8b7
SalmanEssam80 <salman.essam@gmail.com>
2023-10-07 21:27:20 +0200

    create views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,33 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Mobile Specific Metas-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Construction Html5 Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="Themefisher">
+    <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}" />
+
+    <!-- Themefisher Icon font -->
+    <link rel="stylesheet" href="{{ asset('plugins/themefisher-font/style.css') }}">
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
+
+    <!-- Animate css -->
+    <link rel="stylesheet" href="{{ asset('plugins/animate/animate.css') }}">
+    <!-- Slick Carousel -->
+    <link rel="stylesheet" href="{{ asset('plugins/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/slick/slick-theme.css') }}">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -13,68 +38,232 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+    <section class="top-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="contact-number">
+                        <i class="tf-ion-ios-telephone"></i>
+                        <span>0123-123-12312</span>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <!-- Site Logo -->
+                    <div class="logo text-center">
+                        <a href="{{ url('/') }}">
+                            <svg width="135px" height="29px" viewBox="0 0 155 29" version="1.1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
+                                    font-size="40" font-family="AustinBold, Austin" font-weight="bold">
+                                    <g id="Group" transform="translate(-108.000000, -297.000000)" fill="#000000">
+                                        <text id="AVIATO">
+                                            <tspan x="108.94" y="325">Sellers</tspan>
+                                        </text>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <!-- Cart -->
+                    <ul class="top-menu text-right list-inline">
+                        <li class="dropdown cart-nav dropdown-slide">
+                            <a href="#!" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"><i
+                                    class="tf-ion-android-cart"></i>Cart</a>
+                            <div class="dropdown-menu cart-dropdown">
+                                <!-- Cart Item -->
+                                <div class="media">
+                                    <a class="pull-left" href="#!">
+                                        <img class="media-object" src="image/shop/cart/cart-1.jpg" alt="image" />
                                     </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading"><a href="#!">Ladies Bag</a></h4>
+                                        <div class="cart-price">
+                                            <span>1 x</span>
+                                            <span>1250.00</span>
+                                        </div>
+                                        <h5><strong>$1200</strong></h5>
+                                    </div>
+                                    <a href="#!" class="remove"><i class="tf-ion-close"></i></a>
+                                </div><!-- / Cart Item -->
+                                <!-- Cart Item -->
+                                <div class="media">
+                                    <a class="pull-left" href="#!">
+                                        <img class="media-object" src="image/shop/cart/cart-2.jpg" alt="image" />
+                                    </a>
+                                    <div class="media-body">
+                                        <h4 class="media-heading"><a href="#!">Ladies Bag</a></h4>
+                                        <div class="cart-price">
+                                            <span>1 x</span>
+                                            <span>1250.00</span>
+                                        </div>
+                                        <h5><strong>$1200</strong></h5>
+                                    </div>
+                                    <a href="#!" class="remove"><i class="tf-ion-close"></i></a>
+                                </div><!-- / Cart Item -->
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+                                <div class="cart-summary">
+                                    <span>Total</span>
+                                    <span class="total-price">$1799.00</span>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                <ul class="text-center cart-buttons">
+                                    <li><a href="cart.html" class="btn btn-small">View Cart</a></li>
+                                    <li><a href="checkout.html" class="btn btn-small btn-solid-border">Checkout</a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </li><!-- / Cart -->
+
+                        <!-- Search -->
+                        <li class="dropdown search dropdown-slide">
+                            <a href="#!" class="dropdown-toggle" data-toggle="dropdown"
+                                data-hover="dropdown"><i class="tf-ion-ios-search-strong"></i> Search</a>
+                            <ul class="dropdown-menu search-dropdown">
+                                <li>
+                                    <form action="post"><input type="search" class="form-control"
+                                            placeholder="Search..."></form>
+                                </li>
+                            </ul>
+                        </li><!-- / Search -->
+
+                    </ul><!-- / .nav .navbar-nav .navbar-right -->
                 </div>
             </div>
-        </nav>
+        </div>
+    </section>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <section class="menu">
+        <nav class="navbar navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <h2 class="menu-title">Main Menu</h2>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                </div><!-- / .navbar-header -->
+
+                <!-- Navbar Links -->
+                <div id="navbar" class="navbar-collapse collapse text-center">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown ">
+                            <a href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('shop') }}">Shop</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('blog') }}">Blog</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('products') }}">Products</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('user_dashboard') }}">Dashboard</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('contact_us') }}">Contact</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('about_us') }}">About</a>
+                        </li>
+                        <li class="dropdown ">
+                            <a href="{{ route('faq') }}">FAQ</a>
+                        </li>
+                    </ul><!-- / .nav .navbar-nav -->
+                </div>
+            </div><!-- / .container -->
+        </nav>
+    </section>
+
+    @yield('content')
+
+    <footer class="footer section text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="social-media">
+                        <li>
+                            <a href="https://www.facebook.com">
+                                <i class="tf-ion-social-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com">
+                                <i class="tf-ion-social-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.twitter.com">
+                                <i class="tf-ion-social-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.pinterest.com">
+                                <i class="tf-ion-social-pinterest"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="footer-menu text-uppercase">
+                        <li>
+                            <a href="contact.html">CONTACT</a>
+                        </li>
+                        <li>
+                            <a href="shop.html">SHOP</a>
+                        </li>
+                        <li>
+                            <a href="pricing.html">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">PRIVACY POLICY</a>
+                        </li>
+                    </ul>
+                    <p class="copyright-text">Copyright &copy;2023, Designed &amp; Developed by <a
+                            href="https://github.com/SalmanEssam80">Salman Essam</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Main jQuery -->
+    <script src="{{ asset('plugins/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!-- Bootstrap Touchpin -->
+    <script src="{{ asset('plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js') }}"></script>
+    <!-- Instagram Feed Js -->
+    <script src="{{ asset('plugins/instafeed/instafeed.min.js') }}"></script>
+    <!-- Video Lightbox Plugin -->
+    <script src="{{ asset('plugins/ekko-lightbox/dist/ekko-lightbox.min.js') }}"></script>
+    <!-- Count Down Js -->
+    <script src="{{ asset('plugins/syo-timer/build/jquery.syotimer.min.js') }}"></script>
+
+    <!-- slick Carousel -->
+    <script src="{{ asset('plugins/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('plugins/slick/slick-animation.min.js') }}"></script>
+
+    <!-- Google Mapl -->
+    <script src="{{ asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw') }}">
+    </script>
+    <script type="text/javascript" src="{{ asset('plugins/google-map/gmap.js') }}"></script>
+
+    <!-- Main Js File -->
+    <script src="{{ asset('js/script.js') }}"></script>
+
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::view('/thankYou','confirmation')->name('thanks_for_shoping');
+Route::view('/dashboard','user_dashboard')->name('user_dashboard');
+Route::view('/products','product')->name('products');
+Route::view('/blog','blog')->name('blog');
+Route::view('/addresses','addresses')->name('addresses');
+Route::view('/order','orders')->name('order');
+Route::view('/shop','shop')->name('shop');
+Route::view('/checkout','checkout')->name('checkout');
+Route::view('/about-us','about')->name('about_us');
+Route::view('/profile','profile')->name('user_profile');
+Route::view('/contact-us','contact_us')->name('contact_us');
+Route::view('/faq','faq')->name('faq');
+
 Route::get('/', function () {
     return view('welcome');
 });
